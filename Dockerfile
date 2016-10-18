952b5c6c8712f649fc5b9c4fc850e01e0e1d99f3
Toon Van Dooren <toonvd@localhost.localdomain>
2016-10-18 16:05:05 +0200

    backup script

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM alpine:3.4
+MAINTAINER Toon van Dooren <toon@weepee.io>
+
+ADD scripts/backup.sh /scripts/backup.sh
+
+RUN apk upgrade && \
+ apk add --update bash postgresql-client mysql-client \
+ chmod -R a+rx /scripts && \
+ rm -f /var/cache/apk/*
+
+WORKDIR /scripts
+
+ENTRYPOINT ["/scripts/backup.sh"]
+
+# Set labels used in OpenShift to describe the builder images
+LABEL io.k8s.description="DB Backup" \
+      io.k8s.display-name="DB Bacup" \
+      io.openshift.expose-services="" \
+      io.openshift.min-memory="1Gi" \
+      io.openshift.min-cpu="1" \
+      io.openshift.non-scalable="false"
---
+++ scripts/backup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+function backupDB{
+
+    # move to backup folder
+    cd /data/backup;
+
+    # dump DB
+    if [[ $DB_TYPE -eq 'MYSQL' ]]
+    then
+        mysqldump -u $DB_USER -p$($DB_PASSWORD) -h $MYSQL_SERVICE_HOST $DB_NAME | gzip -c > $(date +%Y-%m-%d-%H.%M.%S).sql.gz;
+    else
+        pg_dump -h  -Fc $DB_NAME > $(date +%Y-%m-%d-%H.%M.%S).dump
+    fi
+
+    #delete oldest backup file
+    rm "$(ls -t | tail -1)";
+}
+
+function checkTimeAndBackupIfMatched {
+    CUR_HOUR=$(date +"%H");
+    CUR_MIN=$(date +"%M");
+
+    #get time input
+    set -- "$TIME";
+    IFS=":"; declare -a REQUESTED_TIME=($*);
+
+    if [[ ${REQUESTED_TIME[0]} -eq CUR_HOUR && ${REQUESTED_TIME[0]} -eq CUR_MIN ]]
+    then
+        backupDB;
+    fi
+
+    sleep 1m;
+}
+
+function backupAndSetSleep {
+    backupDB;
+    sleep $($TIME)m;
+}
+
+# cron stuf
+while true
+do
+    if [[ $DAILY_BACKUP == 1 ]]
+    then
+        checkTimeAndBackupIfMatched;
+    else
+        executeAndSetSleep;
+    fi
+done
